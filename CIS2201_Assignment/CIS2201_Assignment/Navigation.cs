d3fe47d8d52c113182a06f65ff7bff53cd399769
Philip Paul Grima <philippaul-2002@outlook.com>
2021-12-13 16:52:56 +0100

    Navigation form

--- CIS2201_Assignment/CIS2201_Assignment/Navigation.cs
+++ CIS2201_Assignment/CIS2201_Assignment/Navigation.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CIS2201_Assignment
 {
-    public partial class Form1 : Form
+    public partial class Navigation : Form
     {
-        public Form1()
+        public Navigation()
         {
             InitializeComponent();
         }
