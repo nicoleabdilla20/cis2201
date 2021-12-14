ea011c07f3f06858238133e601c7273628d98e0c
nicoleabdilla20 <nicole.abdilla.20@um.edu.mt>
2021-12-14 14:49:36 +0100

    Navigation buttons
    Added some code to navigation buttons

--- CIS2201_Assignment/CIS2201_Assignment/Navigation.cs
+++ CIS2201_Assignment/CIS2201_Assignment/Navigation.cs
@@ -19,7 +19,14 @@ namespace CIS2201_Assignment
 
         private void btnPatients_Click(object sender, EventArgs e)
         {
+              Form frm = new Patients();
+                frm.Show();
+        }
 
+        private void btnStaff_Click(object sender, EventArgs e)
+        {
+              Form frm = new Staff();
+              frm.Show();
         }
     }
 }
